bed60cbaa59cc91a6eb5aac0dd2f636e23d307b1
james2811 <phamquanghoang2811@gmail.com>
2020-07-02 02:30:05 +0000

    Whyred: Bring up DerpFest

--- derp_whyred.mk
+++ derp_whyred.mk
@@ -19,16 +19,24 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
-# Inherit some common Stag stuff
-$(call inherit-product, vendor/stag/main.mk)
+# Inherit some common SynthOS stuff
+$(call inherit-product, vendor/aosip/config/common_full_phone.mk)
 
 # Inherit from whyred device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
+# Inherit GApps Stuffs
+TARGET_GAPPS_ARCH := arm64
+IS_PHONE := true
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# Inherit some SynthOS specifically stuffs
+DERP_BUILDTYPE := Synthetic
+
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := whyred
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_NAME := stag_whyred
+PRODUCT_NAME := derp_whyred
 PRODUCT_MODEL := Redmi Note 5 Pro
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
